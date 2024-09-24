c42e13fdc1d904ed7a44d00c1d4526b2e7fcdcf5
Phil Chiu <whophilchiu@gmail.com>
2024-09-24 08:07:05 -0600

    Copy BUILD_PREFIX vtk executables into PREFIX

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,26 +1,32 @@
 #!/bin/sh
-# copy/pasted from https://github.com/conda-forge/visan-feedstock/blob/0f57597d811646486019d0beee56f39269e87e21/recipe/build.sh#L6-L27
+
+# # copy/pasted from https://github.com/conda-forge/visan-feedstock/blob/0f57597d811646486019d0beee56f39269e87e21/recipe/build.sh#L6-L27
+# if test "${CONDA_BUILD_CROSS_COMPILATION}" == "1"; then
+#   # This wrapper script is needed for the VTK build tools when cross compiling.
+#   # When e.g. vtkWrapPython is invoked from the target conda environment in
+#   # $PREFIX, CMake will pass the command invocation to this wrapper script.
+#   # We then redirect the call to the executable in the build conda environment
+#   # in $BUILD_PREFIX (which will be of the proper host architecture).
+#   cat << _EOF > crosswrapper.sh
+# #!/bin/bash
+# executable=\$1
+# shift
+# echo "PREFIX=\$PREFIX"
+# echo "BUILD_PREFIX=\$BUILD_PREFIX"
+# echo "executable=\$executable"
+# if [[ \$executable == "\$PREFIX"* ]] ; then
+#   executable=\$BUILD_PREFIX\${executable#\$PREFIX}
+#   echo "executable=\$executable"
+# fi
+# \$executable \$@
+# _EOF
+#   chmod 755 crosswrapper.sh
+#   CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CROSSCOMPILING_EMULATOR=${PWD}/crosswrapper.sh"
+# fi
+
+
 if test "${CONDA_BUILD_CROSS_COMPILATION}" == "1"; then
-  # This wrapper script is needed for the VTK build tools when cross compiling.
-  # When e.g. vtkWrapPython is invoked from the target conda environment in
-  # $PREFIX, CMake will pass the command invocation to this wrapper script.
-  # We then redirect the call to the executable in the build conda environment
-  # in $BUILD_PREFIX (which will be of the proper host architecture).
-  cat << _EOF > crosswrapper.sh
-#!/bin/bash
-executable=\$1
-shift
-echo "PREFIX=\$PREFIX"
-echo "BUILD_PREFIX=\$BUILD_PREFIX"
-echo "executable=\$executable"
-if [[ \$executable == "\$PREFIX"* ]] ; then
-  executable=\$BUILD_PREFIX\${executable#\$PREFIX}
-  echo "executable=\$executable"
-fi
-\$executable \$@
-_EOF
-  chmod 755 crosswrapper.sh
-  CMAKE_ARGS="${CMAKE_ARGS} -DCMAKE_CROSSCOMPILING_EMULATOR=${PWD}/crosswrapper.sh"
+    cp $BUILD_PREFIX/bin/vtk* $PREFIX/bin/
 fi
 
 set -euo pipefail

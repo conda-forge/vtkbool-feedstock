47d0905944d72abd2bd4dc9857211b3c4dfb7f5f
Phil Chiu <whophilchiu@gmail.com>
2025-08-04 08:31:19 -0600

    Remove extra flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,13 +6,6 @@ rm -rf build
 # Use bash "Remove Largest Suffix Pattern" to get rid of all but major version number
 PYTHON_MAJOR_VERSION=${PY_VER%%.*}
 
-EXTRA_CMAKE_FLAGS=""
-if [[ "$(uname)" == "Darwin" ]]; then
-  export CXXFLAGS="${CXXFLAGS} -stdlib=libc++"
-  export LDFLAGS="${LDFLAGS} -lc++ -lc++abi"
-  EXTRA_CMAKE_FLAGS="-DCMAKE_CXX_FLAGS='${CXXFLAGS}' -DCMAKE_EXE_LINKER_FLAGS='${LDFLAGS}'"
-fi
-
 cmake -B build -S . -G "Ninja" \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_PREFIX_PATH:PATH="${PREFIX}" \
@@ -23,8 +16,7 @@ cmake -B build -S . -G "Ninja" \
     -DVTK_PYTHON_VERSION:STRING="${PYTHON_MAJOR_VERSION}" \
     -DPython3_FIND_STRATEGY=LOCATION \
     -DPython3_ROOT_DIR=${PREFIX} \
-    -DPython3_EXECUTABLE=${PREFIX}/bin/python \
-    ${EXTRA_CMAKE_FLAGS}
+    -DPython3_EXECUTABLE=${PREFIX}/bin/python
 
 cmake --build build -j${CPU_COUNT}
 cmake --install build
